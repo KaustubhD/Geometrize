824cc75615e7efe0ef92f4059633d4f51abf6905
KaustubhD <kaustubhd287@gmail.com>
2018-08-22 23:29:02 +0530

    Just making

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let output = document.getElementById('output')
 let typesRadio = document.querySelectorAll('input[name="type"]')
+let left = output.querySelector('#left')
+let raster = output.querySelector('#raster')
+let vector = output.querySelector('#vector')
 console.log(typesRadio)
 
 function init(){
   // let typesRadio = document.querySelectorAll('input[name="type"]')
+  // output.style.display = 'block'
   typesRadio.forEach(rad => {
     rad.addEventListener('click', syncRadios)
   })
@@ -33,16 +37,20 @@ function syncRangesLabels(range){
 }
 
 function submitForm(e){
+  e.preventDefault()
   let fileIn = document.getElementById('file-input')
   let fileURL = document.getElementById('file-url')
   let temp = 'nothing'
   if(fileIn.length > 0){
-    url = URL.createObjectURL(fileIn.files[0])
+    temp = URL.createObjectURL(fileIn.files[0])
   }
   else if(fileURL.value){
-    url = fileURL.value
+    temp = fileURL.value
   }
-
+  let config = getConfig()
+  config.temp = temp
+  console.log(config)
+  newLeftCanvas(config).then(leftCanvas => newRightCanvas(leftCanvas, config))
 }
 
 let numericProps = ['stepSize', 'alpha', 'viewSize', 'computeSize', 'mutateTimes', 'beginShapes']
@@ -68,5 +76,126 @@ function getConfig(){
   })
 
   return config
+}
 
+function diffIt(one, other){
+  let sum = 0
+  for(let i = 0; i < one.data.length; i++){
+    if(i % 4 == 3){ continue }
+    sum += Math.pow((other.data[i] - one.data[i]), 2)
+  }
+  return sum
+}
+
+function diff_dist(diff, pix){
+  // I don't really understand the logic
+  return Math.sqrt(diff / (3 * pix)) / 255
+}
+
+function dist_diff(dist, pix){
+  // This as well
+  return Math.pow(dist * 255, 2) * (3 * pix)
+}
+
+class Canvas{
+  constructor(width, height){
+    this.canvas = document.createElement('canvas')
+    this.canvas.width = width
+    this.canvas.height = height
+    this.ctx = this.canvas.getContext('2d')
+    this.imageData = null
+    fillFull(this.)
+  }
+
+  static newLeftCanvas(config){
+    if(config.temp == 'nothing'){ return Promise.resolve(this.newTestCanvas(config))}
+    else{
+      return new Promise(resolve => {
+        let image = new Image()
+        image.crossOrigin = true
+        image.src = config.temp
+        image.onload = e => {
+          let computeScale = getScale(image.naturalWidth, image.naturalWidth, config.computeSize)
+          let viewScale = getScale(image.naturalWidth, image.naturalWidth, config.viewSize)
+          config.width = image.naturalWidth / computeScale
+          config.height = image.naturalHeight / computeScale
+          config.scale = computeScale / viewScale
+
+          let canvas = new Canvas(config.width, config.height)
+          canvas.ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+
+          resolve(canvas)
+        }
+        image.onerror = e => {
+          console.log(e)
+          console.log('An error occured in loading the picture')
+        }
+      })
+    }
+  }
+
+  static newTestCanvas(config){
+    let [w, h] = [config.computeSize, config.computeSize]
+    let canvas = new Canvas(w, h)
+    canvas.fillFull('#fff')
+    let ctx = canvas.ctx
+
+    //Make test shape
+    ctx.fillStyle="#f00"
+    ctx.beginPath()
+    ctx.arc(w / 4, h / 2, w / 7, 0, 2 * Math.PI, true)
+    ctx.fill()
+
+    ctx.fillStyle="#0f0"
+    ctx.beginPath()
+    ctx.arc(w / 2, h / 2, w / 7, 0, 2 * Math.PI, true)
+    ctx.fill()
+
+    ctx.fillStyle="#00f"
+    ctx.beginPath()
+    ctx.arc((3 * w) / 4, h / 2, w / 7, 0, 2 * Math.PI, true)
+    ctx.fill()
+
+    return canvas
+  }
+
+  static newRightCanvas(leftCanvas, config){
+    
+    left.appendChild(leftCanvas.canvas)
+
+  }
+
+  static fillFull(color){
+    this.ctx.fillStyle = color
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    return this
+  }
+  
+  getImageData(){
+    if(!this.imageData){
+      this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height)
+    }
+    return this.imageData
+  }
+
+  diffCans(otherOne){
+    return diffIt(this.data.getImageData(), otherOne.data.getImageData())
+  }
+
+  distCans(otherOne){
+    let diff = this.diffCans(otherOne)
+    return diff_dist(diff, this.canvas.width * this.canvas.height)
+  }
+
+}
+
+class Optimiser{
+  constructor(leftCanvas, config){
+    this.config = config
+    this.state = new State(leftCanvas, new Canvas)
+  }
+}
+
+function getScale(wid, hei, siz){
+  return Math.max(wid / siz, hei / siz, 1)
 }
